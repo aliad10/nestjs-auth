05ae7d8e3756d83e4203a8dbd2b04e20aaa024c3
smuggler <mahdigorbanzadeh.1378@yahoo.com>
2022-07-17 17:36:01 +0430

    add mongoose package & add user module

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -2,9 +2,31 @@ import { Prop, Schema } from "@nestjs/mongoose";
 
 @Schema()
 export class User {
-  @Prop({ type: String, unique: true, isRequired: true })
+  toLower(email: string): string {
+    return email.toLowerCase();
+  }
+
+  @Prop({ type: String, unique: true, isRequired: true, index: 1 })
   username;
 
-  //   @Prop({ type: String, unique: true, isRequired: true })
-  //   username;
+  @Prop({ type: String, unique: true, isRequired: true })
+  password;
+
+  @Prop({ type: String, unique: true, isRequired: true, immutable: true })
+  phoneNumber;
+
+  @Prop({ type: String })
+  firstName;
+
+  @Prop({ type: String })
+  lastName;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt;
+
+  @Prop({ type: Date, default: Date.now })
+  updatedAt;
+
+  @Prop({})
+  email;
 }
