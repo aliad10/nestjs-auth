cdf2449a081a4b9a3b0866280c3f70903caf8a0a
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-07-17 15:36:23 +0430

    connection to mongodb added

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,20 @@
 import { Module } from "@nestjs/common";
 
-@Module({})
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigService } from "@nestjs/config";
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>("NODE_ENV") === "test"
+            ? configService.get<string>("MONGO_TEST_CONNECTION")
+            : configService.get<string>("MONGO_CONNECTION"),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [],
+  exports: [],
+})
 export class DatabaseModule {}
