ffd27376559dfdb9d7b0a11f034820c36c220ab0
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-07-27 15:22:02 +0430

    cinfig testing with request

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,78 @@
+// import { INestApplication, ValidationPipe } from "@nestjs/common";
+// import { Test, TestingModule } from "@nestjs/testing";
+// import { AppModule } from "./../src/app.module";
+// import { Connection, connect } from "mongoose";
+// import { ConfigModule, ConfigService } from "@nestjs/config";
+// import Web3 from "web3";
+// import { spec } from "pactum";
+
+// describe("App e2e", () => {
+//   let app: INestApplication;
+//   let mongoConnection: Connection;
+//   let config: ConfigService;
+//   let web3: Web3;
+
+//   beforeAll(async () => {
+//     const moduleRef: TestingModule = await Test.createTestingModule({
+//       imports: [AppModule, ConfigModule.forRoot({ isGlobal: true })],
+//       providers: [ConfigService],
+//     }).compile();
+
+//     config = moduleRef.get<ConfigService>(ConfigService);
+
+//     web3 = new Web3(
+//       `https://rinkeby.infura.io/v3/${config.get("INFURA_PROJECT_ID")}`
+//     );
+
+//     mongoConnection = (await connect(config.get("MONGO_TEST_CONNECTION")))
+//       .connection;
+
+//     app = moduleRef.createNestApplication();
+
+//     app.useGlobalPipes(
+//       new ValidationPipe({
+//         whitelist: true,
+//       })
+//     );
+
+//     await app.init();
+//     await app.listen(3333);
+//   });
+
+//   afterAll(async () => {
+//     await mongoConnection.dropDatabase();
+//     await mongoConnection.close();
+//     app.close();
+//   });
+
+//   afterEach(async () => {
+//     const collections = mongoConnection.collections;
+//     for (const key in collections) {
+//       const collection = collections[key];
+//       await collection.deleteMany({});
+//     }
+//   });
+
+//   it("get nonce", async () => {
+//     //-----create account
+
+//     let account = await web3.eth.accounts.create();
+
+//     let test = await spec().get(
+//       `http://localhost:3333/auth/nonce/${account.address}`
+//     );
+
+//     console.log("xxxxxxxxxxxxx", test);
+//   });
+// });
+
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "./../src/app.module";
 import { Connection, connect } from "mongoose";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import Web3 from "web3";
+import request from "supertest";
 import { spec } from "pactum";
 
 describe("App e2e", () => {
@@ -11,6 +80,7 @@ describe("App e2e", () => {
   let mongoConnection: Connection;
   let config: ConfigService;
   let web3: Web3;
+  let httpServer: any;
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -21,7 +91,7 @@ describe("App e2e", () => {
     config = moduleRef.get<ConfigService>(ConfigService);
 
     web3 = new Web3(
-      `https://rinkeby.infura.io/v3/${config.get("INFURA_PROJECT_ID")}`,
+      `https://rinkeby.infura.io/v3/${config.get("INFURA_PROJECT_ID")}`
     );
 
     mongoConnection = (await connect(config.get("MONGO_TEST_CONNECTION")))
@@ -29,10 +99,12 @@ describe("App e2e", () => {
 
     app = moduleRef.createNestApplication();
 
+    httpServer = app.getHttpServer();
+
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
-      }),
+      })
     );
 
     await app.init();
@@ -40,17 +112,17 @@ describe("App e2e", () => {
   });
 
   afterAll(async () => {
-    await mongoConnection.dropDatabase();
+    // await mongoConnection.dropDatabase();
     await mongoConnection.close();
-    app.close();
+    await app.close();
   });
 
   afterEach(async () => {
-    const collections = mongoConnection.collections;
-    for (const key in collections) {
-      const collection = collections[key];
-      await collection.deleteMany({});
-    }
+    // const collections = mongoConnection.collections;
+    // for (const key in collections) {
+    //   const collection = collections[key];
+    //   await collection.deleteMany({});
+    // }
   });
 
   it("get nonce", async () => {
@@ -58,10 +130,7 @@ describe("App e2e", () => {
 
     let account = await web3.eth.accounts.create();
 
-    let test = await spec().get(
-      `http://localhost:3333/auth/nonce/${account.address}`,
-    );
-
-    console.log(test);
+    let x = await request(httpServer).get(`/auth/nonce/${account.address}`);
+    console.log("xxxxxxxxxxxxx", x.body);
   });
 });
